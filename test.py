47a16e5eccfb6d0b5469d5a6c038645afb7294d9
Altan <altankayabasi3@gmail.com>
2025-10-04 21:13:22 +0200

    chore: add .gitignore and project structure

---
+++ test.py
@@ -0,0 +1,51 @@
+# test.py
+
+def main():
+    print("Welcome to your my Python project!")
+
+if __name__ == "__main__":
+    main()
+
+    # global config (if not set) 
+    # git config --global user.name "Altan" 
+   # git config --global user.email "altankayabasi3@gmail.com"
+
+  # md data\raw data\processed notebooks src figures reports
+# Python
+__pycache__/
+*.py[cod]
+
+# Jupyter
+.ipynb_checkpoints/
+
+# Virtual envs
+.venv/
+env/
+venv/
+
+# OS / editors
+.DS_Store
+.vscode/
+.idea/
+
+# Data (if you don't want to commit raw data)
+data/raw/
+# Python
+__pycache__/
+*.py[cod]
+
+# Jupyter
+.ipynb_checkpoints/
+
+# Virtual envs
+.venv/
+env/
+venv/
+
+# OS / editors
+.DS_Store
+.vscode/
+.idea/
+
+# Data (if you don't want to commit raw data)
+data/raw/
